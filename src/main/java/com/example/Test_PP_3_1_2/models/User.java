f1a3cda8c5dc840f7d2ce9e64efe9b3018d9b9cf
Айхан Касумов <akasum99@gmail.com>
2024-02-28 04:15:54 +0400

    решение (2)

--- src/main/java/com/example/Test_PP_3_1_2/models/User.java
+++ src/main/java/com/example/Test_PP_3_1_2/models/User.java
@@ -22,7 +22,7 @@ public class User {
     private String email;
     @Column
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
     @JoinTable(
             name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
